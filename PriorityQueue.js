c34e7383342746ec3d1f86d05e237dac56a26a37
Jing Wang <5525046+msfirebird@users.noreply.github.com>
2022-10-05 00:25:53 -0700

    Create PriorityQueue.js

---
+++ PriorityQueue.js
@@ -0,0 +1,23 @@
+class PriorityQueue {
+  constructor() {
+    this.heap = [null]
+  }
+  
+  insert(value, priority) {
+    const newNode = new Node(value, priority);
+    this.heap.push(newNode);
+    let currentNodeIdx = this.heap.length - 1;
+    let currentNodeParentIdx = Math.floor(currentNodeIdx / 2);
+    while (
+      this.heap[currentNodeParentIdx] &&
+      newNode.priority > this.heap[currentNodeParentIdx].priority
+    ) {
+      const parent = this.heap[currentNodeParentIdx];
+      this.heap[currentNodeParentIdx] = newNode;
+      this.heap[currentNodeIdx] = parent;
+      currentNodeIdx = currentNodeParentIdx;
+      currentNodeParentIdx = Math.floor(currentNodeIdx / 2);
+    }
+  }
+  
+}
